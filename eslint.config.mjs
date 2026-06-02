0102d2cad13706070a1d9404a9ae30c3bd2c57d5
TimK42 <timk42@github.com>
2026-06-03 02:30:27 +0800

    feat: add ESLint and Prettier CI checks

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import globals from 'globals';
+import js from '@eslint/js';
+
+export default [
+  {
+    ignores: ['node_modules/', 'public/', 'eslint.config.mjs'],
+  },
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+        ...globals.commonjs,
+      },
+    },
+    rules: {
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      semi: ['error', 'always'],
+      'no-unused-vars': ['warn', { args: 'none' }],
+      'no-console': 'off',
+      'no-empty': ['error', { allowEmptyCatch: true }],
+      'comma-dangle': ['error', 'always-multiline'],
+      'no-trailing-spaces': 'error',
+      'eol-last': ['error', 'always'],
+      'no-var': 'error',
+      'prefer-const': 'error',
+    },
+  },
+];
